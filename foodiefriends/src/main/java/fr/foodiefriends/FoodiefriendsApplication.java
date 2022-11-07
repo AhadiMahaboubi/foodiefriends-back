8ab64f739649ca32e25f11389dc94b89cec3aaaf
AhadiMahaboubi <AhadiMahaboubi>
2022-11-07 15:00:56 +0100

    First Commit : Spring Starter Project & Maven Option

---
+++ foodiefriends/src/main/java/fr/foodiefriends/FoodiefriendsApplication.java
@@ -0,0 +1,13 @@
+package fr.foodiefriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiefriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiefriendsApplication.class, args);
+	}
+
+}
---
+++ foodiefriends/src/test/java/fr/foodiefriends/FoodiefriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.foodiefriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiefriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
